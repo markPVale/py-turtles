ea8902856138d85b8887dedebb40cd6d2533adb8
Mark-Vale <markpvale@gmail.com>
2020-11-26 10:03:53 -0500

    add exercise 1

--- Chap_10_Event-Driven-Programming.py
+++ Chap_10_Event-Driven-Programming.py
@@ -145,65 +145,159 @@ import turtle
 
 #  10.4 An Example: State Machines:
 
-turtle.setup(400, 500)
-wn = turtle.Screen()
-wn.title("Tess becomes a traffic light!")
-wn.bgcolor("lightgreen")
-tess = turtle.Turtle()
+# turtle.setup(400, 500)
+# wn = turtle.Screen()
+# wn.title("Tess becomes a traffic light!")
+# wn.bgcolor("lightgreen")
+# tess = turtle.Turtle()
 
 
-def draw_housing():
-    """ Draw a nice housing to hold the traffic lights """
-    tess.pensize(3)
-    tess.color("black", "darkgrey")
-    tess.begin_fill()
-    tess.forward(80)
-    tess.left(90)
-    tess.forward(200)
-    tess.circle(40, 180)
-    tess.forward(200)
-    tess.left(90)
-    tess.end_fill()
+# def draw_housing():
+#     """ Draw a nice housing to hold the traffic lights """
+#     tess.pensize(3)
+#     tess.color("black", "darkgrey")
+#     tess.begin_fill()
+#     tess.forward(80)
+#     tess.left(90)
+#     tess.forward(200)
+#     tess.circle(40, 180)
+#     tess.forward(200)
+#     tess.left(90)
+#     tess.end_fill()
 
 
-draw_housing()
+# draw_housing()
 
-tess.penup()
-# Position tess onto the place where the green light should be
-tess.forward(40)
-tess.left(90)
-tess.forward(50)
-# Turn tess into a big green circle
-tess.shape("circle")
-tess.shapesize(3)
-tess.fillcolor("green")
+# tess.penup()
+# # Position tess onto the place where the green light should be
+# tess.forward(40)
+# tess.left(90)
+# tess.forward(50)
+# # Turn tess into a big green circle
+# tess.shape("circle")
+# tess.shapesize(3)
+# tess.fillcolor("green")
 
-# A traffic light is a kind of state machine with three states,
-# Green, Orange, Red.  We number these states 0, 1, 2
-#  When the machine changes state, we change tess' position and her fillcolor
+# # A traffic light is a kind of state machine with three states,
+# # Green, Orange, Red.  We number these states 0, 1, 2
+# #  When the machine changes state, we change tess' position and her fillcolor
 
-# This variable holds the current state of the machine
-state_num = 0
-
-
-def advance_state_machine():
-    global state_num
-    if state_num == 0:
-        tess.forward(70)
-        tess.fillcolor("orange")
-        state_num = 1
-    elif state_num == 1:
-        tess.forward(70)
-        tess.fillcolor("red")
-        state_num = 2
-    else:
-        tess.back(140)
-        tess.fillcolor("green")
-        state_num = 0
+# # This variable holds the current state of the machine
+# state_num = 0
 
 
-wn.onkey(advance_state_machine, "space")
+# def advance_state_machine():
+#     global state_num
+#     if state_num == 0:
+#         tess.forward(70)
+#         tess.fillcolor("orange")
+#         state_num = 1
+#     elif state_num == 1:
+#         tess.forward(70)
+#         tess.fillcolor("red")
+#         state_num = 2
+#     else:
+#         tess.back(140)
+#         tess.fillcolor("green")
+#         state_num = 0
 
+
+# wn.onkey(advance_state_machine, "space")
+
+# wn.listen()
+# turtle.Screen().exitonclick()
+# wn.mainloop()
+
+
+#  10.6 Exercises
+
+# 1) Add some new key bindings to the first sample program:
+
+# Pressing keys R, G or B should change tess’ color to Red, Green or Blue.
+# Pressing keys + or - should increase or decrease the width of tess’ pen.
+# Ensure that the pen size stays between 1 and 20 (inclusive).
+
+
+import turtle
+
+turtle.setup(400, 500)                # Determine the window size
+wn = turtle.Screen()                 # Get a reference to the window
+wn.title("Handling keypresses!")     # Change the window title
+wn.bgcolor("lightgreen")             # Set the background color
+tess = turtle.Turtle()               # Create our favorite turtle
+
+
+# The next four functions are our "event handlers".
+
+
+def h1():
+    tess.forward(30)
+
+
+def h2():
+    tess.left(45)
+
+
+def h3():
+    tess.right(45)
+
+
+def t_red():
+    tess.color("red")
+
+
+def t_green():
+    tess.color("green")
+
+
+def t_blue():
+    tess.color("blue")
+
+
+def twu():
+    size = tess.width()
+    increase = size + 1
+    while size <= 20:
+        tess.width(increase)
+
+
+def twd():
+    size = tess.width()
+    decrease = size - 1
+    while size > 1:
+        tess.width(decrease)
+
+
+def shapey():
+    screen = turtle.Screen()
+    sides = screen.textinput(
+        "would you like to make a line?", "enter num of sides 1-9: ")
+    for i in range(int(sides)):
+        tess.forward(20)
+        tess.left(20)
+    turtle.listen()
+
+
+def h4():
+    wn.bye()                        # Close down the turtle window
+
+
+# These lines "wire up" keypresses to the handlers we've defined.
+wn.onkey(h1, "Up")
+wn.onkey(h2, "Left")
+wn.onkey(h3, "Right")
+wn.onkey(h4, "q")
+wn.onkey(t_red, "r")
+wn.onkey(t_green, "g")
+wn.onkey(t_blue, "b")
+wn.onkey(twu, '+')
+wn.onkey(twd, "-")
+wn.onkey(shapey, "1")
+
+
+# Now we need to tell the window to start listening for events,
+# If any of the keys that we're monitoring is pressed, its
+# handler will be called.
 wn.listen()
-turtle.Screen().exitonclick()
+wn.exitonclick()
 wn.mainloop()
