9b1f7a2e5e55133ec0f34e184e923ffe3ab08ac0
Mark-Vale <markpvale@gmail.com>
2020-12-14 09:37:52 -0500

    add excercise 10 & 11

--- vectors.py
+++ vectors.py
@@ -59,15 +59,67 @@ assert dot_product([1, 2, 1], [1, 4, 3]) == 12
 
 
 def cross_product(u, v):
-    # cx = u[1]*v[2] - u[2]*v[1]
-    # cy = u[2]*v[0] - u[0]*v[2]
-    # cz = u[0]*v[1] - u[1]*v[0]
-    # return [cx, cy, cz]
+    cx = u[1]*v[2] - u[2]*v[1]
+    cy = u[2]*v[0] - u[0]*v[2]
+    cz = u[0]*v[1] - u[1]*v[0]
+    return [cx, cy, cz]
 
-    # np.array(u)
-    # np.array(v)
     return np.cross(u, v)
 
 
 print(cross_product([1, 3, 2], [2, 1, 4]))
 print(cross_product([2, 3, 4], [5, 6, 7]))
+
+
+# 9. Describe the relationship between " ".join(song.split()) and song in the fragment
+# of code below. Are they the same for all strings assigned to song? When would they be
+#  different?
+
+song = "The rain in Spain..."
+
+print(" ".join(song.split()))
+
+print(song.split())
+
+# " ".join(song.split()) splits each item in a string by white space into an array,
+# then joins each item with a separation of white space into one string object. This would
+# return a similar pattern for all string objects where items are separated by white space.
+# This would be different if the string items were seperated by something other than
+# whitespace.
+
+
+# 10. Write a function replace(s, old, new) that replaces all occurrences
+# of old with new in a string s:
+
+def replace(s, old, new):
+    return new.join(s.split(old))
+
+
+print(replace("Mississippi", "i", "I"))
+s = "I love spom! Spom is my favorite food. Spom, spom, yum!"
+assert replace(
+    s, "om", "am") == "I love spam! Spam is my favorite food. Spam, spam, yum!"
+assert replace(
+    s, "o", "a") == "I lave spam! Spam is my favarite faad. Spam, spam, yum!"
+
+
+# 11. Suppose you want to swap around the values in two variables.
+# You decide to factor this out into a reusable function, and write this code:
+
+
+def swap(x, y):      # Incorrect version
+    print("before swap statement: x:", x, "y:", y)
+    (x, y) = (y, x)
+    print("after swap statement: x:", x, "y:", y)
+
+
+a = ["This", "is", "fun"]
+b = [2, 3, 4]
+# print("before swap function call: a:", a, "b:", b)
+swap(a, b)
+# print("after swap function call: a:", a, "b:", b)
+
+
+# The reason this code isn't fully correct deals with scopes. x & y are are local variables
+# inside the function. They get swapped for values within the function and then it's the
+# value that is returned, not the x, y variable itself.
